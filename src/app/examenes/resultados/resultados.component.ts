2c6ead49fcef4003b65c440baf3845ff165d19bf
FelipeCBO <manosalva1432@gmail.com>
2019-03-05 09:16:32 -0300

    exportar a pdf

--- src/app/examenes/resultados/resultados.component.ts
+++ src/app/examenes/resultados/resultados.component.ts
@@ -4,8 +4,6 @@ import { DataAPiService } from 'src/services/data-api.service';
 import * as jsPDF from 'jspdf';
 import * as html2canvas from 'html2canvas';
 
-declare var xepOnline:any;
-
 @Component({
   selector: 'app-resultados',
   templateUrl: './resultados.component.html',
@@ -15,32 +13,23 @@ export class ResultadosComponent implements OnInit {
   
   generarPDF(){
     html2canvas(document.getElementById('contenido'), {
-       // Opciones
-       allowTaint: true,
-       useCORS: false,
-       // Calidad del PDF
-       scale: 1
-    }).then(function(canvas) {
-    var img = canvas.toDataURL("image/png");
-    var doc = new jsPDF();
-    doc.addImage(img,'PNG',7, 20, 195, 105);
-    doc.save('postres.pdf');
+      allowTaint: true,
+      useCORS: false,
+      scale: 1
+      }).then(function(canvas) {
+      var img = canvas.toDataURL("image/png");
+      var doc = new jsPDF();
+      doc.addImage(img,'PNG',7, 20, 195, 105);
+      doc.save('datospdf.pdf');
    });
 }
-
   @Input ('datos') book:FeriadosInterface;
-  
   @ViewChild('datatable') content :  ElementRef;
   datatable: any;
-  
+
   constructor(public dataApi: DataAPiService) { }
   public books: FeriadosInterface;
-  
   public ngOnInit(): void{
-  
-   
-  
-    
     this.getListFeriados();
     this.datatable= $(this.datatable.nativeElement);
     this.datatable.dataTable();
@@ -49,12 +38,9 @@ export class ResultadosComponent implements OnInit {
   getListFeriados() {
     this.dataApi.getAllFeriados().
     subscribe((books: FeriadosInterface) => (this.books = books));
-    
   }
   
   onPreUpdateFeriado (book: FeriadosInterface): void{
     this.dataApi.selectedFeriado = Object.assign({}, book);
   }
- 
- 
 }
