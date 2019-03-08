3a471af82b39947f5c5c01645d51fd5d1b87f433
FelipeCBO <manosalva1432@gmail.com>
2019-03-08 17:32:14 -0300

    git viernes

--- src/app/modaldatos/cargadatos/cargadatos.component.ts
+++ src/app/modaldatos/cargadatos/cargadatos.component.ts
@@ -3,6 +3,9 @@ import { DataAPiService } from '../../../services/data-api.service';
 import * as jsPDF from 'jspdf';
 import * as html2canvas from 'html2canvas';
 import {Location} from '@angular/common';
+import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
+import { FormGroup, FormBuilder } from '@angular/forms';
+
 
 @Component({
   selector: 'app-cargadatos',
@@ -10,6 +13,8 @@ import {Location} from '@angular/common';
   styleUrls: ['./cargadatos.component.css']
 })
 export class CargadatosComponent implements OnInit {
+  form: FormGroup;
+
   PDF(){
     html2canvas(document.getElementById('contenidomodal'), {
       allowTaint: true,
@@ -24,11 +29,25 @@ export class CargadatosComponent implements OnInit {
 }
   constructor(
     public dataApi: DataAPiService, 
-    public location: Location)
+    public location: Location,
+    public fb : FormBuilder )
+    
   {
+this.formControlls();
+  }
+  formControlls()
+  {
+    this.form = this.fb.group({
+        desc:'',
+
+    })
 
   }
 
+  Guardar()
+  {
+       
+  }
   ngOnInit() {
   }
 
